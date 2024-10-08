a404992c0b048bd12f976ca3242225e212b241e2
kowserahmed121 <kowser.dev@gmail.com>
2024-10-09 00:12:48 +0600

    complete all nav & card section

--- js/script.js
+++ js/script.js
@@ -4,14 +4,45 @@ const loadFunction = () => {
     .then((data) => getData(data.categories));
 };
 
+const loadVideo = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => getVideo(data.videos));
+};
+
+const getVideo = (videos) => {
+  const container = document.getElementById("video-container");
+  videos.forEach((video) => {
+    const div = document.createElement("div");
+    div.classList = " card card-compact bg-base-100  shadow-xl";
+    div.innerHTML = `
+     <figure class="h-[200px]">
+    <img
+      src=${video.thumbnail}
+      class="w-full h-full object-cover"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+    `;
+    container.append(div);
+  });
+};
+
 const getData = (category) => {
-    const myId = document.getElementById("catagroy");
+  const myId = document.getElementById("catagroy");
   category.forEach((item) => {
     const btn = document.createElement("button");
-    btn.classList.add("btn")
+    // btn.classList.add("btn", "btn-warning" ,"text-white") // single class add
+    btn.classList = "btn btn-warning text-white";
     btn.innerText = item.category;
-    myId.append(btn)
+    myId.append(btn);
   });
 };
 
 loadFunction();
+loadVideo();
